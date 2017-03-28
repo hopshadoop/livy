e1c53e1379a3113536191fb5f154304f88975c2d
Jim Dowling <jdowling@sics.se>
2017-03-28 13:25:15 +0200

    fetch from remote master before building

--- deploy-snurran.sh
+++ deploy-snurran.sh
@@ -8,6 +8,10 @@ fi
 
 VERSION=`grep -o -a -m 1 -h -r "version>.*</version" pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
 
+git checkout master
+git fetch upstream
+git merge upstream/master
+
 SPARK_VERSION=$1
 echo "Livy version is: $VERSION . Spark version is: $SPARK_VERSION"
 
