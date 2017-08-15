27a013101519276ca69715d7d10de346a9a86711
Jim Dowling <jdowling@sics.se>
2017-08-15 08:21:06 +0200

    cleanup

--- deploy-snurran.sh
+++ deploy-snurran.sh
@@ -2,20 +2,21 @@
 set -e
 
 if [ $# -lt 1 ] ; then
-  echo "Usage: $0 SPARK_VERSION [test]  (e.g., 2.1.0)"
+  echo "Usage: $0 SPARK_VERSION [test]  (e.g., 2.2)"
   exit 1
 fi
 
 VERSION=`grep -o -a -m 1 -h -r "version>.*</version" pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
 
-git checkout master
-git fetch upstream
-git merge upstream/master
+#git checkout master
+#git fetch upstream
+#git merge upstream/master
 
 SPARK_VERSION=$1
 echo "Livy version is: $VERSION . Spark version is: $SPARK_VERSION"
 
-mvn clean -DskipTests -Pspark=$SPARK_VERSION package
+#mvn clean -DskipTests -Pspark-$SPARK_VERSION package
+mvn -DskipTests -Pspark-$SPARK_VERSION package
 
 if [ $# -gt 1 ] ; then
   if [ "$2" == "test" ] ; then
